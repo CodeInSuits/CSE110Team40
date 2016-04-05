7069ffd71674b8de56f70431a2396e58b46f4a07
ChrisRaj <crutkrisht9@gmail.com>
2016-04-05 12:25:48 -0700

    Create assignment_p1.py
    Initial File

---
+++ assignment_p1.py
@@ -0,0 +1,14 @@
+__author__ = 'urajkuma@ucsd.edu,A91060509,yil261@ucsd.edu,PID,L1Kong@ucsd.edu,PID'
+
+def getPossibleActions(currentPrime):
+    return listOfPrimes
+
+def getPath(startingPrime, finalPrime):
+    return path
+
+def main():
+    primes=str(sys.stdin.readline()).split()
+    print(getPath(primes[0], primes[1]))
+
+if __name__ == '__main__':
+    main()
