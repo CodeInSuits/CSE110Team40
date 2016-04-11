248761fda7f52cb128995ee4c7494ef64a9fef2a
Lingyi Kong <dreamcaller@gmail.com>
2016-04-10 21:39:14 -0700

    Updated the comments

--- assignment_p1.py
+++ assignment_p1.py
@@ -3,18 +3,24 @@ __author__ = 'urajkuma@ucsd.edu,A91060509,yil261@ucsd.edu,PID,L1Kong@ucsd.edu,PI
 from Queue import Queue
 
 million = 1000000
-prime = [True]*million #Initialize all numbers to be "prime"
+prime = [True]*million
 
+
+# Sieve of Eratosthenes method for prime number list
+# Create a list where the index is the number we want to if it's prime
+# First mark 0 and 1 as False, and everything else True
+# Then from 2 to limit, for each True value, mark off all its multiple indices as False
+# Return the list
 def sieve(limit):
-    prime = [True]*(limit+1) # create limit+1 list because it's '0' starting
+    prime = [True]*(limit+1)
     prime[0] = False
     prime[1] = False
-    for i in xrange(2,limit+1): # Search through all number up till this number, using "xrange" because it's python 2
-	    if prime[i] == True: # if this number hasn't been marked as False, then it's a prime #
-		    for j in xrange(i+i, limit+1, i): # Find all multiples of this prime #
-			    prime[j] = False # mark off unprime #
+    for i in xrange(2,limit+1):
+        if prime[i] == True:
+	    for j in xrange(i+i, limit+1, i):
+                prime[j] = False
     return prime
-    
+
 def numDigits(num):
 	return len(str(num))
 
