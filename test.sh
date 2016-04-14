a93bc7c6fa729a569ee7273c30aabb2bfdc805ee
Lingyi Kong <dreamcaller@gmail.com>
2016-04-14 16:49:07 -0700

    Add test case and test script

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# run as "./test.sh PYTHON_SCRIPT"
+# require "Test Cases.txt" file in the same directory
+input=$1
+if [[ ! -a $input ]]
+then
+    echo "usage: ./$0 PYTHON_SCRIPT"
+    exit
+fi
+while read line
+do
+    if [[ -z $line || ${line:0:1} == "#" ]]
+    then 
+        continue
+    else
+        echo $line | python $input
+    fi
+done < "Test Cases.txt"
