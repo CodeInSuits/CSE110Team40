777c17fa8930626fd1fd24e1535c13cd24d3000e
Utkrisht Rajkumar <utkrisht96@gmail.com>
2016-04-14 22:17:47 -0700

    part 1

--- assignment_p1.py
+++ assignment_p1.py
@@ -1,86 +1,78 @@
-__author__ = 'urajkuma@ucsd.edu,A91060509,yil261@ucsd.edu,PID,L1Kong@ucsd.edu,PID'
+__author__ = 'urajkuma@ucsd.edu,A91060509,yil261@ucsd.edu,A91085115,L1Kong@ucsd.edu,A97010449'
 
 from Queue import Queue
 
 million = 1000000
-prime = [True]*million
+prime = [False]*million
 
-
-# Sieve of Eratosthenes method for prime number list
-# Create a list where the index is the number we want to if it's prime
-# First mark 0 and 1 as False, and everything else True
-# Then from 2 to limit, for each True value, mark off all its multiple indices as False
-# Return the list
-def sieve(limit):
-    prime = [True]*(limit+1)
-    prime[0] = False
-    prime[1] = False
-    for i in xrange(2,limit+1):
-        if prime[i] == True:
-	    for j in xrange(i+i, limit+1, i):
-                prime[j] = False
-    return prime
-
+def sieve():
+    for i in range(2, 1000, 1):
+        if prime[i] == False:
+            for j in range(i*2, million, i):
+                prime[j] = True
+    
 def numDigits(num):
-	return len(str(num))
+    return len(str(num))
 
 def arrToNum(digits, size):
-	temp = 0
-	x = 0
-	while x < size:
-		temp = temp*10 + digits[x]
-		x+=1
+    temp = 0
+    x = 0
+    while x < size:
+        temp = temp*10 + digits[x]
+        x+=1
 
-	return temp
+    return temp
 
 def arr(digits, num):
-	x = numDigits(num) - 1
-	while num:
-		digits[x] = num%10
-		num = num/10
-		x -= 1
+    x = numDigits(num) - 1
+    while num:
+        digits[x] = num%10
+        num = num/10
+        x -= 1
 
 def exponents(size):
-	threshold = 1
-	while size > 1:
-		threshold *= 10
-		size -= 1
+    threshold = 1
+    while size > 1:
+        threshold *= 10
+        size -= 1
 
-	return threshold
+    return threshold
 
 
 
 
 def main():
-    sieve()
-    parent = [-1]*100000
-    distance = [-1]*100000
     b,a = map(int, raw_input().split())
     size = numDigits(a)
     digits = [None]*size
     primes = Queue()
-    distance[a] = 0
     primes.put(a)
-    parent[a] = 0;
     threshold = exponents(size)
+    sieve()
+    parent = [-1]*million
+    distance = [-1]*million
+    parent[a] = 0;
+    distance[a] = 0    
     while (primes.empty() != True):
-    	num = primes.get()
-    	for x in range((size-1), -1, -1):
-    		arr(digits, num)
-    		for i in range(0, 10):
-    			digits[x] = i
-    			temp = arrToNum(digits, size)
-    			if ((prime[temp] == 1) and (distance[temp] == -1) and (temp >= threshold)):
-    				distance[temp] = distance[num] + 1
-    				parent[temp] = num
-    				primes.put(temp)
-    if(parent[b] != -1):
-        while (parent[b] != 0) and (parent[b] != -1):
-            print b, " "
-            b = parent[b]
+        num = primes.get()
+        for x in range((size-1), -1, -1):
+            arr(digits, num)
+            for i in range(0, 10):
+                digits[x] = i
+                temp = arrToNum(digits, size)
+                if ((prime[temp] == False) and (distance[temp] == -1) and (temp >= threshold)):
+                    distance[temp] = distance[num] + 1
+                    parent[temp] = num
+                    primes.put(temp)
+
+    if(distance[b] == -1):
+        print "UNSOLVABLE"
     else:
-        print 'UNSOLVABLE'
+        while(parent[b] != False):
+            print b,
+            b = parent[b]
+        print a
+
 
 if __name__ == '__main__':
     main()
-
