000d646948eca6cbbeafc71bab8de0abf57f49a9
JaceLu <Luyinji0830@gmail.com>
2016-04-14 22:00:51 -0700

    tentative 5a

--- assignment_p5a.py
+++ assignment_p5a.py
@@ -6,6 +6,7 @@ __author__ = 'urajkuma@ucsd.edu,A91060509,yil261@ucsd.edu,PID,L1Kong@ucsd.edu,PI
 import sys # for arugment parsing
 import heapq
 import itertools
+from time import time
 
 class PriorityQueue:
 		def __init__(self):
@@ -20,7 +21,7 @@ class PriorityQueue:
 		def get(self):
 				return heapq.heappop(self.elements)[1]
 				
-def getPossibleActions2 (num):
+def getPossibleActions(num):
 	# input: <str> num
 	# return a list of <str> num' which are primes and
 	# differ from num by exactly one digit
@@ -32,14 +33,17 @@ def getPossibleActions2 (num):
 		for digit in xrange(N):
 			save_digit = old[digit]
 			for i in xrange(10):
+				if save_digit == i:
+					continue
 				old[digit] = i
 				new = digits2num(old)
 				# no leading zero
-				if str(new) == old:
+				if str(int(new)) == new:
 					if prime[int(new)]:
 						l+=[new]
 			old[digit] = save_digit
-		return l 
+		return l            
+		
 
 def sieve(limit):
 # Sieve of Eratosthenes method for prime number list
@@ -55,6 +59,8 @@ def sieve(limit):
 						for j in xrange(i+i, limit, i):
 								prime[j] = False
 		return prime
+
+
 def num2digits(num):
 		l=[]
 		for i in num:
@@ -69,28 +75,7 @@ def digits2num(l):
 		for i in l:
 				num+=str(i)
 		return num
-						
-def getPossibleActions (num):
-				# input: <str> num
-				# return a list of <str> num' which are primes and
-				# differ from num by exactly one digit
-				# prime[]: prime number lookup table to check if prime[i] is prime
-
-						N = len(num)
-						old = num2digits(num)
-						prime = sieve(10**N)
-						l=[]
-						if not prime[int(num)]:
-							return l
-						for digit in xrange(N):
-								save_digit = old[digit]
-								for i in xrange(10):
-										old[digit] = i
-										new = digits2num(old)
-										if prime[int(new)]:
-												l+=[new]
-								old[digit] = save_digit
-						return l             
+					 
 
 def printPath(came_from, start, goal):
 				if goal not in came_from:
@@ -107,33 +92,41 @@ def heuristic(str1, str2):
 	return sum(itertools.imap(str.__ne__, str1, str2))
 
 def getPath(start, goal):
-				frontier = PriorityQueue()
-				frontier.put(start, 0)
-				came_from = {}
-				cost_so_far = {}
-				came_from[start] = None
-				cost_so_far[start] = 0
-				
-				while not frontier.empty():
-						current = frontier.get()
-						
-						if current == goal:
-								break
-						
-						for element in getPossibleActions(str(current)):
-								next = int(element)
-								new_cost = cost_so_far[current] + 1
-								if next not in cost_so_far or new_cost < cost_so_far[next]:
-										cost_so_far[next] = new_cost
-										priority = new_cost + heuristic(str(goal), str(next))
-										frontier.put(next, priority)
-										came_from[next] = current
-				
-				return came_from
+	N=len(str(start))
+	global prime
+	prime = sieve(10**N)
+	frontier_queue = PriorityQueue()
+	frontier_queue.put(start, 0)
+	came_from_map = {}
+	cost_so_far_map = {}
+	came_from_map[start] = None
+	cost_so_far_map[start] = 0
+	visited = 0
+	
+	while not frontier_queue.empty():
+			current = frontier_queue.get()
+			visited += 1
+			if current == goal:
+					break
+			
+			for element in getPossibleActions(str(current)):
+					next = int(element)
+					new_cost = cost_so_far_map[current] + 1
+					if next not in cost_so_far_map or new_cost < cost_so_far_map[next]:
+							cost_so_far_map[next] = new_cost
+							priority = new_cost + heuristic(str(goal), str(next))
+							frontier_queue.put(next, priority)
+							came_from_map[next] = current
+	print(visited)
+	return came_from_map
 
-#argv=str(sys.stdin.readline()).split()
-came_from = getPath(103, 113)
-print(printPath(came_from, 103, 111))
-print(getPossibleActions(str(113)))
-primes = sieve(200)
-print(primes[103])
+def main() :
+	argv=str(sys.stdin.readline()).split()
+	begin_time=time()
+	came_from = getPath(argv[0], argv[1])
+	print(printPath(came_from, argv[0], argv[1]))
+	end_time=time()
+	print("time spent: ",end_time-begin_time)
+
+if __name__ == '__main__':
+	main()
