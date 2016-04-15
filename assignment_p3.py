e1a3195d08f66c38952902633d9293edd1269cbc
JaceLu <Luyinji0830@gmail.com>
2016-04-14 21:39:40 -0700

    tentative 5b

--- assignment_p3.py
+++ assignment_p3.py
@@ -1,9 +1,49 @@
+#!/usr/bin/python
+
 __author__ = 'urajkuma@ucsd.edu,A91060509,yil261@ucsd.edu,PID,L1Kong@ucsd.edu,PID'
 
 # library importing
 import sys # for arugment parsing
+import heapq
+import itertools
 from time import time
 
+class PriorityQueue:
+		def __init__(self):
+				self.elements = []
+		
+		def empty(self):
+				return len(self.elements) == 0
+		
+		def put(self, item, priority):
+				heapq.heappush(self.elements, (priority, item))
+		
+		def get(self):
+				return heapq.heappop(self.elements)[1]
+				
+def getPossibleActions(num):
+	# input: <str> num
+	# return a list of <str> num' which are primes and
+	# differ from num by exactly one digit
+	# prime[]: prime number lookup table to check if prime[i] is prime
+
+		N = len(num)
+		old = num2digits(num)
+		l=[]
+		for digit in xrange(N):
+			save_digit = old[digit]
+			for i in xrange(10):
+				if save_digit == i:
+					continue
+				old[digit] = i
+				new = digits2num(old)
+				# no leading zero
+				if str(int(new)) == new:
+					if prime[int(new)]:
+						l+=[new]
+			old[digit] = save_digit
+		return l            
+		
 
 def sieve(limit):
 # Sieve of Eratosthenes method for prime number list
@@ -11,166 +51,80 @@ def sieve(limit):
 # First mark 0 and 1 as False, and everything else True
 # Then from 2 to limit, for each True value, mark off all its multiple indices as False
 # Return the list
-
-    tmp = [True] * limit
-    tmp[0] = False
-    tmp[1] = False
-    for i in xrange(2,limit):
-        if tmp[i]:
-            for j in xrange(i+i, limit, i):
-                tmp[j] = False
-    return tmp
-
+		prime = [True]*(limit)
+		prime[0] = False
+		prime[1] = False
+		for i in xrange(2,limit):
+				if prime[i] == True:
+						for j in xrange(i+i, limit, i):
+								prime[j] = False
+		return prime
 def num2digits(num):
-# input: string form of number
-# return the list which contains every digits of the given number
-# least significant digits are stored in the highest index
-
-    l=[]
-    for i in num:
-        l+=[int(i)]
-    return l
-
+		l=[]
+		for i in num:
+				l+=[int(i)]
+		return l
 
 def digits2num(l):
-# input: list of digits
-# convert a list of digits to the string form number
-# where least significant digits are stored in the highest index
+				# input: list of digits
+				# convert a list of digits to the string form number
+				# where least significant digits are stored in the highest index
+		num=""
+		for i in l:
+				num+=str(i)
+		return num
+					 
 
-    num=""
-    for i in l:
-        num+=str(i)
-    return num
+def printPath(came_from, start, goal):
+				if goal not in came_from:
+					return 'UNSOLVABLE'
+				current = goal
+				path = [current]
+				while current != start:
+						current = came_from[current]
+						path.append(current)
+				path.reverse()
+				return path
 
+def heuristic(str1, str2):
+	return 1.5*sum(itertools.imap(str.__ne__, str1, str2))
 
-def getPossibleActions (num):
-# input: <str> num
-# return a list of <str> num' which are primes and
-# differ from num by exactly one digit
-# prime[]: prime number lookup table to check if prime[i] is prime
-
-    N = len(num)
-    old = num2digits(num)
-    l=[]
-    for digit in xrange(N):
-        save_digit = old[digit]
-        for i in xrange(10):
-            # skip same bit
-            if save_digit == i:
-                continue
-            old[digit] = i
-            new = digits2num(old)
-            # no leading zero
-            if new[0] != '0':
-                if prime[int(new)]:
-                    l+=[new]
-        old[digit] = save_digit
-    return l             
-    
-
-def printPath(visit,num):
-# input: visit list, ending number
-# print the path from x->y
-
-    N = len(num)
-    path=num[::-1]
-    prev = visit[num][0]
-    while prev:
-        path = path + " " + prev[::-1]
-        prev = visit[prev][0]
-    return path[::-1].strip()
-
-
-def getPath (p1,p2):
-# input: <str> p1 and <str> p2
-# return the path between two primes
-# using DLS with max-depth=8 and root-depth=0
-# return "UNSOLVABLE" if not path exist
-# visit[]: visited state lookup table to check if prime[i] is visited
-#           as well as storing path information
-#           unvisited entry stores itself
-#           visited entry stores its parent
-
-    N=len(p1)
-    global prime
-    prime = sieve(10**N)
-    if not prime [int(p1)]:
-        return "UNSOLVABLE"
-    if p1 == p2:
-        return p2
-    DEPTH = 8+1
-    for depth in xrange(1,DEPTH):
-        visit = {p1:('',0)} # using '' to indicate root node
-        stack=[(p1,0)]
-        while len(stack) != 0:
-            node, d = stack.pop()
-            for child in getPossibleActions(node):
-                if child == p2:
-                    visit[child] = (node,d+1)
-                    return printPath(visit,p2)
-                if d==depth-1:
-                        continue
-                if (visit.get(child)!= None):
-                    if (d >= visit[child][1]):
-                        continue
-                visit[child] = (node,d+1)
-                stack+=[(child,d+1)]
-    return "UNSOLVABLE"
-
-######################## Alternative ###################3
-def PrintPath(visit,num):
-    begin=time()
-    N = len(num)
-    path=num[::-1]
-    prev = visit[int(num)][0]
-    while prev:
-        path = path + " " + prev[::-1]
-        prev = visit[int(prev)][0]
-    #path = path+" " + prev.zfill(N)[::-1]
-    end=time()
-    print "print time:",end-begin
-    return path[::-1].strip()
-
-def GetPath(p1,p2):
-    begin=time()
-    N=len(p1)
-    global prime
-    prime = sieve(10**N)
-    if not prime [int(p1)]:
-        return "UNSOLVABLE"
-    if p1 == p2:
-        return p2
-    DEPTH = 8+1
-    for depth in xrange(1,DEPTH):
-        visit = [(None,0)]*10**N # using '' to indicate root node
-        visit[int(p1)]=('',0)
-        stack=[(p1,0)]
-        while len(stack) != 0:
-            node, d = stack.pop()
-            for child in getPossibleActions(node):
-                tmp = int(child)
-                if child == p2:
-                    visit[tmp] = (node,d+1)
-                    end=time()
-                    print "find:",end-begin
-                    return printPath(visit,p2)
-                if (visit[tmp][0]!= None and d >= visit[tmp][1]) or d==depth-1:
-                        continue
-                visit[tmp] = (node,d+1)
-                stack+=[(child,d+1)]
-    end=time()
-    print "find:",end-begin
-    return "UNSOLVABLE"
-########################################################
+def getPath(start, goal):
+	N=len(str(start))
+	global prime
+	prime = sieve(10**N)
+	frontier_queue = PriorityQueue()
+	frontier_queue.put(start, 0)
+	came_from_map = {}
+	cost_so_far_map = {}
+	came_from_map[start] = None
+	cost_so_far_map[start] = 0
+	visited = 0
+	
+	while not frontier_queue.empty():
+			current = frontier_queue.get()
+			visited += 1
+			if current == goal:
+					break
+			
+			for element in getPossibleActions(str(current)):
+					next = int(element)
+					new_cost = cost_so_far_map[current] + 1
+					if next not in cost_so_far_map or new_cost < cost_so_far_map[next]:
+							cost_so_far_map[next] = new_cost
+							priority = new_cost + heuristic(str(goal), str(next))
+							frontier_queue.put(next, priority)
+							came_from_map[next] = current
+	print(visited)
+	return came_from_map
 
 def main() :
-    argv=str(sys.stdin.readline()).split()
-    begin=time()
-    path=getPath(argv[0],argv[1])
-    end=time()
-    print(path)
-    print "time:",end-begin
-
+	argv=str(sys.stdin.readline()).split()
+	begin_time=time()
+	came_from = getPath(argv[0], argv[1])
+	print(printPath(came_from, argv[0], argv[1]))
+	end_time=time()
+	print("time spent: ",end_time-begin_time)
 
 if __name__ == '__main__':
-    main()
+	main()
