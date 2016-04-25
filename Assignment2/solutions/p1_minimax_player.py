0e18bee2398b3d1e7feaa3d436d5cf1b75e72a93
Utkrisht Rajkumar <utkrisht96@gmail.com>
2016-04-25 03:12:33 -0700

    almost done with problem 1

--- Assignment2/solutions/p1_minimax_player.py
+++ Assignment2/solutions/p1_minimax_player.py
@@ -15,4 +15,26 @@ class MinimaxPlayer(Player):
         :param state: State, the current state of the board.
         :return: Action, the next move
         """
+
+        action = None
+        possibleMoves = state.actions()
+        if(len(possibleMoves) == 0):
+            return action
+
+"""
+    1. how to pass in the opponent's move?
+    2. What index do we pass in as a second parameter to ActioN(), when
+    we're already at terminal state? Since terminal state has only empty
+    pits
+    
+"""
+        for move in possibleMoves:
+            newBoard = result(move)
+            #action = move(newBoard)
+            if newBoard.is_win():
+                #return Action(newBoard.player_row, something)
+            else:
+                #call minimax with opponent
+                #continue
+
         raise NotImplementedError("Need to implement this method")
