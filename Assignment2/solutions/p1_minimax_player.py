67c3fb279411b6996f37ce52d43f5fcb279ddd53
Lingyi Kong <dreamcaller@gmail.com>
2016-04-28 10:20:23 -0700

    Code simplified
    Simplified the code so that Min/Max only returns utility value (before
    it also returns the best state).

--- Assignment2/solutions/p1_minimax_player.py
+++ Assignment2/solutions/p1_minimax_player.py
@@ -14,14 +14,14 @@ class MinimaxPlayer(Player):
     def Min(self, state):
         """ 
         Args: Takes in state
-        returns: utility for OUR player and best action for opponent
+        returns: utility for OUR player
         """
 
         #if terminal state, then return utility of that state
         if(state.is_terminal()):
             # utility function returns value for opponent
             # thus we take the opposite value for our player
-            return -state.utility(state.player), None
+            return -state.utility(state.player)
 
         #else, generate new states
         possibleMoves = state.actions()
@@ -39,19 +39,18 @@ class MinimaxPlayer(Player):
 
         for nextMove in possibleMoves:
             newBoard = state.result(nextMove)
-            newScore, tmp = self.Max(newBoard)
+            newScore = self.Max(newBoard)
             if(currentScore > newScore):
                 currentScore = newScore
-                bestAction = nextMove
-        return currentScore, bestAction
+        return currentScore
 
 
     def Max(self, state):
         """ Args: Takes in state
-        returns: Utility for our player and best action for our player
+        returns: Utility for our player
         """
         if(state.is_terminal()):
-            return state.utility(state.player), None
+            return state.utility(state.player)
 
         possibleMoves = state.actions()
 
@@ -67,54 +66,35 @@ class MinimaxPlayer(Player):
 
         for nextMove in possibleMoves:
             newBoard = state.result(nextMove)
-            newScore, tmp = self.Min(newBoard)
+            newScore = self.Min(newBoard)
             if(currentScore < newScore):
                 currentScore = newScore
-                bestAction = nextMove
 
                 
-        return currentScore, bestAction
+        return currentScore
 
     def move(self, state):
         """3
         Calculates the best move from the given board using the minimax
         algorithm.
         :param state: State, the current state of the board.
-        :return: Action, the next move
+        :return: best Action for current plyer
         """
         action = None
         possibleMoves = state.actions()
         if(len(possibleMoves) == 0):
             return action
         bestUtil = -1
-        bestAction = 0
-        #get the inital actions and states from given state
-        for player, idx in enumerate(possibleMoves):
-            newBoard = state.result(idx)
-        """
-                Based on which player's turn it is, call MAX or MIN.
-                This will iterate through the tree and run the minimax
-                algorithm. Then, when the MAX or MIN return all the way
-                back to the move function, check if the util of that 
-                state is a win or lose. Do that for all the initial states
-                and keep track which action will result in a win. Then call
-                Action on that action.
+        bestAction = possibleMoves[0] # default action
+        # running minimax algorithm to find best move
+        # return action with max min_utility
+        for nextMove in possibleMoves:
+            newBoard = state.result(nextMove)
+            newScore = self.Min(newBoard) # We are maximizing the min here
+            if bestUtil < newScore:
+                bestUtil = newScore
+                bestAction = nextMove
                 
-            if(state.player_row == 0):
-                currentUtil = self.Max(newBoard)
-            else:
-                currentUtil = self.Min(newBoard)
-
-            #update which of the initial actions give the best result
-            if(bestUtil < currentUtil):
-                bestUtil = currentUtil
-                bestAction = idx
-
-        action = Action(state.player_row, bestAction)
-        """
-
-        # Call Max to find best action
-        tmp, action = self.Max(state)
-        return action 
+        return bestAction
 
         raise NotImplementedError("Need to implement this method")
