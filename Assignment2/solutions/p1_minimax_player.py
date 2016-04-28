685e8b45ece57e3eb182e8ee1ecb5e954ac342e6
Lingyi Kong <dreamcaller@gmail.com>
2016-04-28 05:52:27 -0700

    part 1 working now
    Pleas see my comments for details. Basically the issue was with our
    state checking in Min() and edge case.

--- Assignment2/solutions/p1_minimax_player.py
+++ Assignment2/solutions/p1_minimax_player.py
@@ -14,52 +14,66 @@ class MinimaxPlayer(Player):
     def Min(self, state):
         """ 
         Args: Takes in state
-        returns: -1,0,1 based on utility of the player of that state 
+        returns: utility for OUR player and best action for opponent
         """
 
         #if terminal state, then return utility of that state
         if(state.is_terminal()):
-            return state.utility(state.player)
+            # utility function returns value for opponent
+            # thus we take the opposite value for our player
+            return -state.utility(state.player), None
 
         #else, generate new states
         possibleMoves = state.actions()
 
         #if no possible moves for Min, then call Max again on that same state
         if(len(possibleMoves) == 0):
-            return self.Max(state)
+			# need to switch opponent when calling Max again
+			# result function with None action simply returns
+			# the same state with opponent
+            return self.Max(state.result(None))
 
-        currentScore = 0 #to keep track of which is the winner at each level
+        # Our opponent wants to have -1 for our player's utility
+        currentScore = 1 # assuming losing position for our opponent
+        bestAction = possibleMoves[0]
 
-        for i, move in enumerate(possibleMoves):
-            newBoard = state.result(move)
-            newScore = self.Max(newBoard)
-            if(newScore >= currentScore):
+        for nextMove in possibleMoves:
+            newBoard = state.result(nextMove)
+            newScore, tmp = self.Max(newBoard)
+            if(currentScore > newScore):
                 currentScore = newScore
-
-        return currentScore
+                bestAction = nextMove
+        return currentScore, bestAction
 
 
     def Max(self, state):
         """ Args: Takes in state
-        returns: -1,0,1 based on utility of the player of that state """
+        returns: Utility for our player and best action for our player
+        """
         if(state.is_terminal()):
-            return state.utility(state.player)
+            return state.utility(state.player), None
 
         possibleMoves = state.actions()
 
         #if no possible moves for Max, then call Min again on that same state
         if(len(possibleMoves) == 0):
-            return self.Min(state)
+			# need to switch opponent when calling Max again
+			# result function with None action simply returns
+			# the same state with opponent
+            return self.Min(state.result(None))
 
-        currentScore = 0 #to keep track of which is winner at each level
+        currentScore = -1 # Initialize losing position
+        bestAction = possibleMoves[0] # If no better option, take first one
 
-        for i, move in enumerate(possibleMoves):
-            newBoard = state.result(move)
-            newScore = self.Min(newBoard)
+        for nextMove in possibleMoves:
+            newBoard = state.result(nextMove)
+            newScore, tmp = self.Min(newBoard)
             if(currentScore < newScore):
                 currentScore = newScore
+                bestAction = nextMove
 
-        return currentScore
+                
+        return currentScore, bestAction
 
     def move(self, state):
         """3
@@ -75,9 +89,9 @@ class MinimaxPlayer(Player):
         bestUtil = -1
         bestAction = 0
         #get the inital actions and states from given state
-        for i, move in enumerate(possibleMoves):
-            newBoard = state.result(move)
-            """
+        for player, idx in enumerate(possibleMoves):
+            newBoard = state.result(idx)
+        """
                 Based on which player's turn it is, call MAX or MIN.
                 This will iterate through the tree and run the minimax
                 algorithm. Then, when the MAX or MIN return all the way
@@ -85,7 +99,7 @@ class MinimaxPlayer(Player):
                 state is a win or lose. Do that for all the initial states
                 and keep track which action will result in a win. Then call
                 Action on that action.
-            """
+                
             if(state.player_row == 0):
                 currentUtil = self.Max(newBoard)
             else:
@@ -94,9 +108,13 @@ class MinimaxPlayer(Player):
             #update which of the initial actions give the best result
             if(bestUtil < currentUtil):
                 bestUtil = currentUtil
-                bestAction = i
+                bestAction = idx
 
-        action = Action(state.player_row, i)
+        action = Action(state.player_row, bestAction)
+        """
+
+        # Call Max to find best action
+        tmp, action = self.Max(state)
         return action 
 
         raise NotImplementedError("Need to implement this method")
