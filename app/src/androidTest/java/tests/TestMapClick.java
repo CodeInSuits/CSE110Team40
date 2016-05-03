95d620b508b9817d81aaab886b7977f114bb4084
Matthew Chinn <mattchinn96@gmail.com>
2016-05-02 19:21:11 -0700

    rename test file

--- app/src/androidTest/java/tests/TestMapClick.java
+++ app/src/androidTest/java/tests/TestMapClick.java
@@ -7,11 +7,14 @@ import com.vapenaysh.jace.myapplication.MapsActivity;
 /**
  * Created by Matt on 5/1/16.
  */
-public class Map_tests extends ActivityInstrumentationTestCase2 {
+public class TestMapClick extends ActivityInstrumentationTestCase2 {
 
     MapsActivity mapsActivity;
 
-    public Map_tests(){
+    public TestMapClick(){
         super(MapsActivity.class);
+        mapsActivity = (MapsActivity) getActivity();
     }
+
+
 }
