bf448a7659b2d51e4438db416476ee98ff1c9f32
bijancfarahani <bijancfarahani@gmail.com>
2016-05-07 17:38:00 -0700

    slight edits to test

--- app/src/androidTest/java/com/vapenaysh/jace/myapplication/AddPartnerTest.java
+++ app/src/androidTest/java/com/vapenaysh/jace/myapplication/AddPartnerTest.java
@@ -1,5 +1,7 @@
 package com.vapenaysh.jace.myapplication;
 
+import android.test.suitebuilder.annotation.MediumTest;
+
 import junit.framework.TestCase;
 
 /**
@@ -14,14 +16,15 @@ public class AddPartnerTest extends TestCase {
     public void testOnCreate() throws Exception {
 
     }
-
-
+    @MediumTest
     public void testGetRegId() throws Exception {
         AddPartner a = new AddPartner();
         a.getRegId();
         assertNotNull(a.gcm);
-    }
+        //suspicion that our code is wrong in its logic
+        assertNotSame(a.gcm, Constants.PROJECT_NUMBER);
 
+    }
     public void testOnClick() throws Exception {
 
     }
