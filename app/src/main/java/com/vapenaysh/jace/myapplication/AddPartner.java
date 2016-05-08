55bfa6d7b2235bd6b0de69850f6168e8e9f2b65c
Estheroy <xuouyang@ucsd.edu>
2016-05-07 17:57:13 -0700

    store partner information

--- app/src/main/java/com/vapenaysh/jace/myapplication/AddPartner.java
+++ app/src/main/java/com/vapenaysh/jace/myapplication/AddPartner.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
@@ -68,6 +69,8 @@ public class AddPartner extends Activity implements View.OnClickListener {
         editor.putString("partner_name",uname.getText().toString());
         editor.putString("phone_number",uemail.getText().toString());
         editor.commit();
+        Toast.makeText(getApplicationContext(), "Partner Added", Toast.LENGTH_SHORT).show();
+
 
     }
 
