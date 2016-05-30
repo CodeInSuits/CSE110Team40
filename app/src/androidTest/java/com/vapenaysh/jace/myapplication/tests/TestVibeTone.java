c35d80e762df552eb575b6795c9cd0855c0b9844
Estheroy <xuouyang@ucsd.edu>
2016-05-29 22:22:00 -0700

    some BDD test cases for VibeTone Settin

---
+++ app/src/androidTest/java/com/vapenaysh/jace/myapplication/tests/TestVibeTone.java
@@ -0,0 +1,17 @@
+package com.vapenaysh.jace.myapplication.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.vapenaysh.jace.myapplication.GlobalSettings;
+import com.vapenaysh.jace.myapplication.VibeToneManager;
+import com.vapenaysh.jace.myapplication.VibeToneSetting;
+
+/**
+ * Created by XuanpeiEstherOuyang on 5/27/16.
+ */
+public class TestVibeTone extends ActivityInstrumentationTestCase2<VibeToneSetting> {
+
+    public TestVibeTone(Class<VibeToneSetting> activityClass) {
+        super(activityClass);
+    }
+}
