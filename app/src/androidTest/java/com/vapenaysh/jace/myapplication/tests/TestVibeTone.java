691d682804db8f34bd3c7b815c0afc768416c05b
Adam Abadilla <adamabadilla@Adams-MacBook-Pro-3.local>
2016-05-30 13:40:50 -0700

    TestVibeTone v1

--- app/src/androidTest/java/com/vapenaysh/jace/myapplication/tests/TestVibeTone.java
+++ app/src/androidTest/java/com/vapenaysh/jace/myapplication/tests/TestVibeTone.java
@@ -1,17 +1,67 @@
 package com.vapenaysh.jace.myapplication.tests;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.vapenaysh.jace.myapplication.GlobalSettings;
 import com.vapenaysh.jace.myapplication.VibeToneManager;
 import com.vapenaysh.jace.myapplication.VibeToneSetting;
+import com.vapenaysh.jace.myapplication.R;
 
 /**
  * Created by XuanpeiEstherOuyang on 5/27/16.
  */
 public class TestVibeTone extends ActivityInstrumentationTestCase2<VibeToneSetting> {
 
-    public TestVibeTone(Class<VibeToneSetting> activityClass) {
-        super(activityClass);
+    public TestVibeTone() {
+        super(VibeToneSetting.class);
     }
+
+    private VibeToneSetting vibeToneSetting;
+
+    public void test_vibeTone() {
+
+        vibeToneSetting = getActivity();
+        vibeToneSetting.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                final RadioButton button1 = (RadioButton) vibeToneSetting.findViewById(R.id.radioButton);
+                final RadioGroup radioGroup = (RadioGroup) vibeToneSetting.findViewById(R.id.myRadioGroup);
+
+                assertNotNull(button1);
+
+                if (button1 == null || radioGroup == null) {
+                    fail("button or radiogroup null");
+                }
+
+                // Check VibeTone1
+                radioGroup.check(button1.getId());
+
+                int checkedId = radioGroup.getCheckedRadioButtonId();
+                final RadioButton checkedButton = (RadioButton)radioGroup.findViewById(checkedId);
+
+                // Assert VibeTone1 is selected
+                assertEquals(checkedButton.getId(), button1.getId());
+
+                // Access Save Button
+                final Button saveButton = (Button) vibeToneSetting.findViewById(R.id.button3);
+                assertNotNull(saveButton);
+                saveButton.callOnClick();
+
+                // Assert that VibeTone1 was saved
+                assertEquals(1, vibeToneSetting.getVibeToneIndex());
+
+            }
+
+        });
+
+    }
+
+
 }
